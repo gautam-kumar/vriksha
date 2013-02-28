13421ef9c0c561fa1ca39255260affded89c0e60
Gautam Kumar <gautamkumar.iit@gmail.com>
2013-02-27 18:22:34 -0800

    Removing CedarSimC#

--- CedarSimC#/Results/29-01-2013/Section 3 Plots/FitMapDurations.R
+++
@@ -1,4 +0,0 @@
-require(rriskDistributions)
-p1=c(0.5, 0.75, 0.9, 0.95, 0.99, 0.999)
-q1=c(87.27, 155.52, 336.76, 572.01, 1260.53, 5518.2)
-fit.perc(p=p1,q=q1,show.output=FALSE, tolPlot=0.1, tolConv=0.001, fit.weights=c(0.3, 0.3, 0.3, 1, 1, 1))
