69f82d461ec42280c5d56cd6d2466ce8e08626fd
Gautam Kumar <gautamk@outlook.com>
2013-08-27 15:53:47 -0700

    Adding AggregateEvaluator

---
+++ SparkImpl/core/src/main/scala/spark/partial/AggregateEvaluator.scala
@@ -0,0 +1,41 @@
+package spark.partial
+
+import cern.jet.stat.Probability
+
+/**
+ * An ApproximateEvaluator for aggregation.
+ *
+ * TODO: There's currently a lot of shared code between this and GroupedCountEvaluator. It might
+ * be best to make this a special case of GroupedCountEvaluator with one group.
+ */
+private[spark] class AggregateEvaluator[T : ClassManifest](totalOutputs: Int)
+  extends ApproximateEvaluator[Array[T], Array[T]] {
+
+  var outputsMerged = 0
+  var sum: Array[T] = new Array[T](0) 
+
+  override def merge(outputId: Int, taskResult: Array[T]) {
+    outputsMerged += 1
+    sum = sum ++ taskResult
+  }
+
+  override def currentResult(): Array[T] = {
+    sum
+    /*
+    if (outputsMerged == totalOutputs) {
+      new BoundedDouble(sum, 1.0, sum, sum)
+    } else if (outputsMerged == 0) {
+      new BoundedDouble(0, 0.0, Double.NegativeInfinity, Double.PositiveInfinity)
+    } else {
+      val p = outputsMerged.toDouble / totalOutputs
+      val mean = (sum + 1 - p) / p
+      val variance = (sum + 1) * (1 - p) / (p * p)
+      val stdev = math.sqrt(variance)
+      val confFactor = Probability.normalInverse(1 - (1 - confidence) / 2)
+      val low = mean - confFactor * stdev
+      val high = mean + confFactor * stdev
+      new BoundedDouble(mean, confidence, low, high)
+    }
+    */
+  }
+}
