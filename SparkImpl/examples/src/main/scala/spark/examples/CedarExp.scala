7ba378cf48ae93e45b454015fbc5ad97b15d2372
root <root@ip-10-30-131-228.ec2.internal>
2013-09-13 05:28:23 +0000

    Added CedarExp

---
+++ SparkImpl/examples/src/main/scala/spark/examples/CedarExp.scala
@@ -0,0 +1,24 @@
+package spark.examples
+
+import spark._
+import SparkContext._
+
+/** Computes an approximation to pi */
+object CedarExp {
+  def main(args: Array[String]) {
+    if (args.length == 0) {
+      System.err.println("Usage: CedarExp <master> <deadline> <useCedar>[<slices>]")
+      System.exit(1)
+    }
+    val spark = new SparkContext(args(0), "CedarExp",
+      System.getenv("SPARK_HOME"), Seq(System.getenv("SPARK_EXAMPLES_JAR")))
+    val deadline = args(1).toInt
+    val u = args(2).toInt
+    var useCedar = true
+    if (u == 0) useCedar = false
+    val pageCount = spark.parallelize(1 to 800, 800)
+    val a = pageCount.cedar(16, deadline, 4.4, 1.15, true, 2.94, 0.52, useCedar).initialValue.size
+    println("a is : " + a + " with useCedar: " + useCedar)
+    System.exit(0)
+  }
+}
